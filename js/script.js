69f9f2bf18193dd731b68a49486fe6a1cfda6fcd
daoudaw <daoudaw@hotmail.com>
2022-05-23 22:46:42 +0200

    adding some UI effects

--- js/script.js
+++ js/script.js
@@ -14,6 +14,13 @@ const da_counter =document.querySelector('#da_counter');
 const bra2_counter =document.querySelector('#bra2_counter');
 const end_counter =document.querySelector('#end_counter');
 
+const a_counter_bis =document.querySelector('#a_counter_bis');
+const bra_counter_bis =document.querySelector('#bra_counter_bis');
+const ca_counter_bis =document.querySelector('#ca_counter_bis');
+const da_counter_bis =document.querySelector('#da_counter_bis');
+const bra2_counter_bis =document.querySelector('#bra2_counter_bis');
+const end_counter_bis =document.querySelector('#end_counter_bis');
+
 //Secret counter
 let counter = 0;
 
@@ -22,25 +29,44 @@ const result = document.querySelector('.result');
 
 //disabling buttons
 bt_bra.disabled = true;
+bt_bra.style.backgroundColor = "#27252f";
+
 bt_ca.disabled = true;
+bt_ca.style.backgroundColor = "#27252f";
+
 bt_da.disabled = true;
+bt_da.style.backgroundColor = "#27252f";
+
 bt_bra2.disabled = true;
+bt_bra2.style.backgroundColor = "#27252f";
+
 bt_end.disabled = true;
+bt_end.style.backgroundColor = "#27252f";
+
 reveal.disabled = true;
+reveal.style.backgroundColor = "#27252f";
 
 // Enabling first button
 bt_a.addEventListener('click', ()=> {
     bt_bra.disabled = false;
+    bt_bra.style.backgroundColor = "white";
     bt_ca.disabled = true;
     bt_da.disabled = true;
     bt_bra2.disabled = true;
     bt_end.disabled = true;
 });
 
-// Disabling the first button
+// Disabling the first button yes
 a_counter.addEventListener('click', ()=> {
     bt_a.disabled = true;
+    bt_a.style.backgroundColor = "#27252f";
     counter += 1;
+});
+// Disabling the first button no
+a_counter_bis.addEventListener('click', ()=> {
+    bt_a.disabled = true;
+    bt_a.style.backgroundColor = "#27252f";
+
 });
 
 
@@ -49,15 +75,23 @@ a_counter.addEventListener('click', ()=> {
 bt_bra.addEventListener('click', ()=> {
     bt_a.disabled = true;
     bt_ca.disabled = false;
+    bt_ca.style.backgroundColor = "white";
     bt_da.disabled = true;
     bt_bra2.disabled = true;
     bt_end.disabled = true;
 })
 
-// Disabling the second button
+// Disabling the second button yes
 bra_counter.addEventListener('click', ()=> {
     bt_bra.disabled = true;
+    bt_bra.style.backgroundColor = "#27252f";
     counter += 2;
+});
+// Disabling the second button no
+bra_counter_bis.addEventListener('click', ()=> {
+    bt_bra.disabled = true;
+    bt_bra.style.backgroundColor = "#27252f";
+   
 });
 
 
@@ -67,14 +101,22 @@ bt_ca.addEventListener('click', ()=> {
     bt_a.disabled = true;
     bt_bra.disabled = true;
     bt_da.disabled = false;
+    bt_da.style.backgroundColor = "white";
     bt_bra2.disabled = true;
     bt_end.disabled = true;
 });
 
-// Disabling the third button
+// Disabling the third button yes
 ca_counter.addEventListener('click', ()=> {
     bt_ca.disabled = true;
+    bt_ca.style.backgroundColor = "#27252f";
     counter += 4;
+});
+// Disabling the third button no
+ca_counter_bis.addEventListener('click', ()=> {
+    bt_ca.disabled = true;
+    bt_ca.style.backgroundColor = "#27252f";
+  
 });
 
 
@@ -85,14 +127,23 @@ bt_da.addEventListener('click', ()=> {
     bt_bra.disabled = true;
     bt_ca.disabled = true;
     bt_bra2.disabled = false;
+    bt_bra2.style.backgroundColor = "white";
     bt_end.disabled = true;
 });
 
-// Disabling the fourth button
+// Disabling the fourth button yes
 da_counter.addEventListener('click', ()=> {
     bt_da.disabled = true;
+    bt_da.style.backgroundColor = "#27252f";
     counter += 8;
 });
+// Disabling the fourth button no
+da_counter_bis.addEventListener('click', ()=> {
+    bt_da.disabled = true;
+    bt_da.style.backgroundColor = "#27252f";
+  
+});
+
 
 // Enabling fifth button
 bt_bra2.addEventListener('click', ()=> {
@@ -101,12 +152,20 @@ bt_bra2.addEventListener('click', ()=> {
     bt_ca.disabled = true;
     bt_da.disabled = true;
     bt_end.disabled = false;
+    bt_end.style.backgroundColor = "white";
 });
 
-// Disabling the fith button
+// Disabling the fith button yes
 bra2_counter.addEventListener('click', ()=> {
     bt_bra2.disabled = true;
+    bt_bra2.style.backgroundColor = "#27252f";
     counter += 16;
+});
+// Disabling the fith button no
+bra2_counter_bis.addEventListener('click', ()=> {
+    bt_bra2.disabled = true;
+    bt_bra2.style.backgroundColor = "#27252f";
+  
 });
 
 
@@ -119,22 +178,42 @@ bt_end.addEventListener('click', ()=> {
     bt_da.disabled = true;
     bt_bra2.disabled = true;
     reveal.disabled = false;
+    reveal.style.backgroundColor = "white";
 });
 
-// Disabling the sixth button
+// Disabling the sixth button yes
 end_counter.addEventListener('click', ()=> {
     bt_end.disabled = true;
+    bt_end.style.backgroundColor = "#27252f";
     counter += 32;
 });
+// Disabling the sixth button no
+end_counter_bis.addEventListener('click', ()=> {
+    bt_end.disabled = true;
+    bt_end.style.backgroundColor = "#27252f";
+   
+});
+
 
 //Poof button
 reveal.addEventListener('click', ()=> {
     bt_a.disabled = true;
+    bt_a.style.backgroundColor = "#27252f";
+
     bt_bra.disabled = true;
+    bt_bra.style.backgroundColor = "#27252f";
+
     bt_ca.disabled = true;
+    bt_ca.style.backgroundColor = "#27252f";
+
     bt_da.disabled = true;
+    bt_da.style.backgroundColor = "#27252f";
+
     bt_bra2.disabled = true;
+    bt_bra2.style.backgroundColor = "#27252f";
+
     bt_end.disabled = true;
+    bt_end.style.backgroundColor = "#27252f";
 
     //if counter > 0, show number
     if(counter > 0) {
